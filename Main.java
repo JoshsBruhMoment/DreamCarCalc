81e318a166312aed8b9542a68950b66ff7326ad2
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2020-10-07 13:05:01 +0000

    added a print statement for explaining what is needed to be typed in.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
    Scanner scan = new scanner (system.in);
-
+System.out.println("Please type your dream car and it's price");
+String name = scan.NextLine();
   }
 }
