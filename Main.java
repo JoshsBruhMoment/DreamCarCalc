d06311361628589ce1bc7945e2b9ba82e4ebb452
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2020-10-12 15:51:37 +0000

    Added new line to look for boolean (yes or no).

--- Main.java
+++ Main.java
@@ -4,8 +4,16 @@ class Main {
   public static void main(String[] args) {
 
    Scanner myObj = new Scanner (System.in);
-System.out.println("Please type your dream car and it's price");
+System.out.println("Please type your dream cars name");
 String name = myObj.nextLine();
+
+System.out.println("Please type your dream cars price");
 double price = myObj.nextDouble();
+
+System.out.println("You are sure you want this car?");
+boolean confirm = myObj.nextBoolean();
+
+if (boolean confirm = yes)
+{System.out.println("Your final choice is " + name "which came out to " + price);}
   }
 }
